fa681289ad84ab475534e5770b57748934aa98db
Andy Lin <alin21@stuy.edu>
2020-12-23 13:21:25 -0500

    added ay thing

--- PigLatin.java
+++ PigLatin.java
@@ -3,6 +3,9 @@ class PigLatin{
     s = s.toLowerCase();
     if (s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' || s.charAt(0) == 'o' || s.charAt(0) == 'u'){
       s += "hay";
+    }else{
+      String first = s.substring(0,1);
+      s = s.substring(1, s.length()) + first + "ay";
     }
     return s;
   }
