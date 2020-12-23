da781311d5f04acb5c1ecf8d6149860982f443ca
Andy Lin <alin21@stuy.edu>
2020-12-23 13:14:30 -0500

    fixed how to get it in

--- MakeStars.java
+++ MakeStars.java
@@ -2,24 +2,19 @@ import java.io.*;
 import java.util.*;
 
 class MakeStars{
-  public static void main(String[] args) throws FileNotFoundException{
-    try{
-      Scanner in = new Scanner(new File(args[0]));
-      while(in.hasNextLine()){
-        Scanner line = new Scanner(in.nextLine());
-        String toPrint = "";
-        while(line.hasNext()){
-          int wordLength = line.next().length();
-          for (int i = 0; i<wordLength; i++){
-            toPrint += "*";
-          }
-          toPrint += " ";
+  public static void main(String[] args) throws IOException{
+    Scanner in = new Scanner(new InputStreamReader(System.in));
+    while(in.hasNextLine()){
+      Scanner line = new Scanner(in.nextLine());
+      String toPrint = "";
+      while(line.hasNext()){
+        int wordLength = line.next().length();
+        for (int i = 0; i<wordLength; i++){
+          toPrint += "*";
         }
-        System.out.println(toPrint);
+        toPrint += " ";
       }
-    }
-    catch (Exception e){
-      throw new Error("Please provide a valid file.");
+      System.out.println(toPrint);
     }
   }
 }
