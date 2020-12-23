0d5b223da0bbc7f0f645b4b3ff1e2fee4c212211
Andy Lin <alin21@stuy.edu>
2020-12-23 13:18:34 -0500

    added hay thing

---
+++ PigLatin.java
@@ -0,0 +1,9 @@
+class PigLatin{
+  public static String pigLatinSimple(String s){
+    s = s.toLowerCase();
+    if (s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' || s.charAt(0) == 'o' || s.charAt(0) == 'u'){
+      s += "hay";
+    }
+    return s;
+  }
+}
