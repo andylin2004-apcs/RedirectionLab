c28111900893697c287e0aaa2c4d5c9c067bf705
Andy Lin <alin21@stuy.edu>
2020-12-23 20:21:52 -0500

    pig latin 3 but no puns yet

--- PigLatin.java
+++ PigLatin.java
@@ -26,4 +26,17 @@ class PigLatin{
     }
     return s;
   }
+  public static String pigLatinBest(String s){
+    s = s.toLowerCase();
+    if (Arrays.asList(vovels).contains(s.charAt(0))){
+      s += "hay";
+    }else if (Arrays.asList(dia).contains(s.substring(0,2))){
+      String first = s.substring(0,2);
+      s = s.substring(2, s.length()) + first + "ay";
+    }else if ((int)s.charAt(0) >= 97 && (int)s.charAt(0) <= 122){
+      String first = s.substring(0,1);
+      s = s.substring(1, s.length()) + first + "ay";
+    }
+    return s;
+  }
 }
