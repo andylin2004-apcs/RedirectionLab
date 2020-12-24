96d33a360d5dfaa1e9f4e1fb0e7b439b32fdee0e
Andy Lin <alin21@stuy.edu>
2020-12-23 20:29:20 -0500

    pig latin 3 with puns

--- PigLatin.java
+++ PigLatin.java
@@ -29,13 +29,24 @@ class PigLatin{
   public static String pigLatinBest(String s){
     s = s.toLowerCase();
     if (Arrays.asList(vovels).contains(s.charAt(0))){
-      s += "hay";
+      if ((int)s.charAt(s.length()-1) < 97 || (int)s.charAt(s.length()-1) > 122){
+        s = s.substring(0, s.length()-1) + "hay" + s.substring(s.length()-1, s.length());
+      }else{
+        s += "hay";
+      }
     }else if (Arrays.asList(dia).contains(s.substring(0,2))){
       String first = s.substring(0,2);
-      s = s.substring(2, s.length()) + first + "ay";
+      if ((int)s.charAt(s.length()-1) < 97 || (int)s.charAt(s.length()-1) > 122){
+        s = s.substring(2, s.length()-1) + s.substring(0,2) + "ay" + s.substring(s.length()-1, s.length());
+      }else{
+        s = s.substring(2, s.length()) + s.substring(0,2) + "ay";
+      }
     }else if ((int)s.charAt(0) >= 97 && (int)s.charAt(0) <= 122){
-      String first = s.substring(0,1);
-      s = s.substring(1, s.length()) + first + "ay";
+      if ((int)s.charAt(s.length()-1) < 97 || (int)s.charAt(s.length()-1) > 122){
+        s = s.substring(1, s.length()-1) + s.substring(0,1) + "ay" + s.substring(s.length()-1, s.length());
+      }else{
+        s = s.substring(1, s.length()) + s.substring(0,1) + "ay";
+      }
     }
     return s;
   }
