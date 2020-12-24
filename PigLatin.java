fb92ff58ce61639e7ef6f209a21d518ffe6d6688
Andy Lin <alin21@stuy.edu>
2020-12-23 20:17:49 -0500

    pig latin 2 eletric bongaloo

--- PigLatin.java
+++ PigLatin.java
@@ -2,10 +2,24 @@ import java.util.*;
 
 class PigLatin{
   static char[] vovels = new char[]{'a','e','i','o','u'};
+  static String[] dia = new String[]{"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
   public static String pigLatinSimple(String s){
     s = s.toLowerCase();
     if (Arrays.asList(vovels).contains(s.charAt(0))){
       s += "hay";
+    }else{
+      String first = s.substring(0,1);
+      s = s.substring(1, s.length()) + first + "ay";
+    }
+    return s;
+  }
+  public static String pigLatin(String s){
+    s = s.toLowerCase();
+    if (Arrays.asList(vovels).contains(s.charAt(0))){
+      s += "hay";
+    }else if (Arrays.asList(dia).contains(s.substring(0,2))){
+      String first = s.substring(0,2);
+      s = s.substring(2, s.length()) + first + "ay";
     }else{
       String first = s.substring(0,1);
       s = s.substring(1, s.length()) + first + "ay";
